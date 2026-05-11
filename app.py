782537b5e1a4e8464ef1b893337f2ce2ae205f67
Kripa-2805 <kripa.mehndiratta2007@gmail.com>
2026-05-11 18:45:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, send_from_directory
+from flask import Flask, render_template
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder=os.path.dirname(os.path.abspath(__file__)))
 
 @app.route('/')
 def index():
-    return send_from_directory('.', 'index.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
